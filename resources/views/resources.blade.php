ec06f0e852c0317b84448808f7b7409907c842b6
jdn-the-dev <jaydon.lynch@hotmail.com>
2024-06-25 22:22:52 -0400

    My-Mind Icon Change

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -2,7 +2,6 @@
 @section('content')
 <div class="d-flex flex-wrap justify-content-center resources-container">
   <resource-card title="Free Wallpaper" resource="wallpaper" icon="wallpaper_slideshow"></resource-card>
-  <resource-card title="PC Parts"></resource-card>
-  <resource-card title="Creativity"></resource-card>
+  <resource-card title="My Mind (Creativity)" resource="my-mind" icon="cognition"></resource-card>
 </div>
 @endsection
