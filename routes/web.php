b4e0f17b6d8951eaceb66ee0dd26d62845ba291f
jdn-the-dev <jaydon.lynch@hotmail.com>
2024-07-14 20:09:49 -0400

    Disable Register Route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageGalleryController;
 
-Auth::routes();
+
+//['register' => false]
+Auth::routes(['register' => false]);
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
