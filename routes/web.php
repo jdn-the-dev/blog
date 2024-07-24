d3191f6dfce3ab2bdcb6b7d2f935fecb4d03fd3e
Jaydon Lynch <jaydon.lynch@hotmail.com>
2024-07-24 14:37:50 -0400

    routes/web.php: add a new route

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,23 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/about', [App\Http\Controllers\AboutController::class, 'index'])->name('about');
 Route::get('/blog', [App\Http\Controllers\PostController::class, 'index'])->name('blog');
 Route::get('/blog/{id?}', [App\Http\Controllers\PostController::class, 'getPost'])->name('blog');
+
 Route::get('/resources', [App\Http\Controllers\ResourcesController::class, 'index'])->name('resources');
 Route::get('/resource/wallpaper', [App\Http\Controllers\ResourcesController::class, 'wallpaperIndex'])->name('resource');
+Route::get('/resource/my-mind', [ImageGalleryController::class, 'index'])->name('my-mind');
+Route::get('/my-mind', [ImageGalleryController::class, 'index'])->name('my-mind');
+
 Route::get('/create-post', [App\Http\Controllers\AdminPostController::class, 'createIndex'])->name('create-post');
 Route::get('/delete/{id?}', [App\Http\Controllers\AdminPostController::class, 'deletePost'])->name('delete-post');
 Route::get('/edit-post/{id}', [App\Http\Controllers\AdminPostController::class, 'editIndex'])->name('edit-post');
 Route::post('/edit-post/{id}', [App\Http\Controllers\AdminPostController::class, 'editPost'])->name('edit-post');
 Route::match(array('POST'), '/store', [App\Http\Controllers\AdminPostController::class, 'store'])->name('store');
 
-Route::get('/resource/my-mind', [ImageGalleryController::class, 'index'])->name('my-mind');
 Route::post('/upload', [ImageGalleryController::class, 'upload'])->name('upload');
 Route::delete('/delete-image/{id}', [ImageGalleryController::class, 'destroy'])->name('image.destroy');
 
 
-
 //Admin
-
 
 Route::get('/admin/posts', [AdminPostController::class, 'adminPostIndex'])->name('admin.posts.index');
 
