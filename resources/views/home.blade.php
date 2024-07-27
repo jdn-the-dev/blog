29019f3301c114c5a4e022de7313a75331b40021
jdn-the-dev <jaydon.lynch@hotmail.com>
2024-07-27 10:40:09 -0400

    Meta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="thumbnail" content="https://www.jaydonlynch.dev/favicon.ico">
         
         <!-- Title and Description -->
         <meta name="description" content="Explore blog posts, tips, and resources on tech/programming. ">
