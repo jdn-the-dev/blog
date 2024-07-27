e3fc71b7d652d47aef78c81b5745d0f4d062e42d
jdn-the-dev <jaydon.lynch@hotmail.com>
2024-07-27 10:33:13 -0400

    Meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,6 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=quicksand" rel="stylesheet">
