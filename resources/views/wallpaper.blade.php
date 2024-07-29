1f238e6a2f3a55e93f30c6423dfbaa8b544c2c51
jdn-the-dev <jaydon.lynch@hotmail.com>
2024-07-29 11:28:30 -0400

    Wallpaper card sizing

--- resources/views/wallpaper.blade.php
+++ resources/views/wallpaper.blade.php
@@ -13,8 +13,8 @@
   <div class="d-flex flex-wrap justify-content-center">
     @if (count($images) > 0)
     @foreach ($images as $image)
-    <div style="width: 300px">
-      <img src="{{ asset('images/' . $image)}}" alt="" srcset="" width="300" height="300">
+    <div class="free-wallpaper" style="width: 300px; margin:1rem">
+      <img src="{{ asset('images/' . $image)}}" alt="" srcset="" width="300" height="250">
       <a class="btn resource-btn" style="width:100%" href="{{ asset('images/' . $image)}}"
       download="proposed_file_name"><i class="fa fa-download"></i> Download</a>
     </div>
