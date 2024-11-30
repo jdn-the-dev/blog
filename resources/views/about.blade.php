949cc0dc456f893ed54f5eae35d887bba6ef303e
jdn-the-dev <jaydon.lynch@hotmail.com>
2024-11-30 11:00:01 -0500

    About page update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,16 +13,15 @@
   @section('content')
   <div class="about-container">
     <span class="about">
-      Hello!<br><br>
-      My name is <b>Jaydon James Amal Lynch</b>. I'm currently a web developer at <a
-        href="https://www.theclearinghouse.org/About/Owner-Banks">The Clearing House</a>. <br><br>
-      On this site, I talk about creativity, code, crypto, as well as
-      some tools and strategies to help programmers navigate the world.
-      <br><br>
-      Personality Type: <b>INFJ-T</b>
+      Hello there!<br><br>
+      I’m <b>Jaydon James Amal Lynch</b>, a web developer at <a href="https://www.theclearinghouse.org/About/Owner-Banks">The Clearing House</a>. Welcome to my little corner of the internet!<br><br>
+      Here, I share thoughts and tips on <b>creativity</b>, <b>coding</b>, <b>crypto</b>, and the tools and strategies that make a programmer's life smoother and more exciting.<br><br>
+      I’m also an <b>INFJ-T</b> personality type!<br><br>
+      And if you're looking for fresh content, I publish a new blog post every month on the <b>last Saturday</b>. There’s always something new to explore, so feel free to check back often!
     </span>
     <div></div>
-    <div class="previous-title"><span>Previous work at</span></div>
+    <br>
+    <div class="previous-title"><span>Previous work at:</span></div>
     <div class="brand-container">
       <div class="brand"><a href="https://www.microsoft.com/en-us/about"><img src="{{ asset('images/microsoft.png') }}"
             alt="microsoft-logo" srcset="">Microsoft</a></div>
@@ -32,7 +31,7 @@
       <div class="brand"><a href="https://www.pasadenaplayhouse.org/about/"><img src="{{ asset('images/pp-logo.png') }}"
             alt="pasadena-playhouse-logo" srcset=""><br>Pasadena Playhouse</a></div>
       <div class="brand"><a href="https://k12.sfsymphony.org/About-SFS"><img src="{{ asset('images/sf-symphony.png') }}"
-            alt="san-franciso-symphony-logo" srcset="" width="" height="">Sanfranciso Symphony</a></div>
+            alt="san-franciso-symphony-logo" srcset="" width="" height="">San Francisco Symphony</a></div>
       <div class="brand"><a href="https://www.total.dev/"><img src="{{asset('images/total-logo.png')}}"
             alt="pasadena-playhouse-logo" srcset="">Total Developers</a></div>
     </div>
