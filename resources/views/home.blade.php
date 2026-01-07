73d5f28b2473b1b74aab092b991787de40b9ce51
jdn-the-dev <jaydon.lynch@hotmail.com>
2026-01-06 20:16:43 -0500

    Updated background - 2026

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,19 +16,19 @@
         
         <!-- Author -->
         <meta name="author" content="Jaydon Lynch">
-        <meta name="thumbnail" content="{{ asset('images/j-train.jpg')}}" />
+        <meta name="thumbnail" content="{{ asset('images/closed-door.jpg')}}" />
         <!-- Open Graph / Facebook -->
         <meta property="og:title" content="Jaydon Lynch: A Developer and Creator">
 <meta property="og:description"
   content="Exploring the intersection of life and productivity, I share insights as a developer and creator. From coding and creative projects to practical tips for living a balanced, productive life, my journey is all about growth, innovation, and inspiring others." />
-        <meta property="og:image" content="{{ asset('images/j-train.jpg')}}" />
+        <meta property="og:image" content="{{ asset('images/closed-door.jpg')}}" />
         <meta property="og:url" content="https://jaydonlynch.dev">
         <meta property="og:type" content="website">
         
         <!-- Twitter -->
         <meta name="twitter:card" content="summary_large_image">
         <meta property="og:title" content="A Developer and Creator.">
-        <meta name="twitter:image" content="{{ asset('images/j-train.jpg')}}" />
+        <meta name="twitter:image" content="{{ asset('images/closed-door.jpg')}}" />
         
         <!-- Robots -->
         <meta name="robots" content="index, follow">
@@ -48,14 +48,16 @@
           margin:0.5rem !important;
           border-radius: 12px;
           height: 98.5%;
-            background: url("{{ asset('images/j-train.jpg') }}") no-repeat 50% fixed;
+            background: url("{{ asset('images/closed-door.jpg') }}") no-repeat center 15% fixed;
             background-size: cover;
         }
         .navbar-brand{
             color: #fff !important;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);
         }
         .nav-link {
             color: #fff !important;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);
         }
         .nav-link:hover {
           text-decoration-color: #fff !important;
